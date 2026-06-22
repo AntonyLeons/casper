eeb322a4ef28ec0ca9230ccbd7d5e2ce83cefe6b
Antony Leons <antony@leons.dev>
2026-06-22 20:24:56 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,11 +16,7 @@ export const SITE_CONFIG = {
   googleAnalytics: 'UA-69281367-1',
   wordsPerMinute: 200,
   pageSize: 25,
-  algolia: {
-    applicationId: '08WJSG8GMW',
-    indexName: 'jasper2',
-    searchOnlyApiKey: '899d0b22cae5a8affeb4f1f9f35adcc4',
-  },
+  algolia: false,
 };
 
 /**
